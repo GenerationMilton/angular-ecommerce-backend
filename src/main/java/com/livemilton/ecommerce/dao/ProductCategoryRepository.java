f425e7b3a70647131c7f142092086e977235bfe5
milton munoz <livemilton@hotmail.com>
2025-01-29 22:17:38 -0500

    add repository to add products

---
+++ src/main/java/com/livemilton/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.livemilton.ecommerce.dao;
+
+import com.livemilton.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
