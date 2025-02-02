a2cef07a97cc7114e56914d0605e0e1048668d4c
milton munoz <livemilton@hotmail.com>
2025-02-02 17:20:30 -0500

    spring boot update param by categoryId

--- src/main/java/com/livemilton/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/livemilton/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.livemilton.ecommerce.dao;
 
 import com.livemilton.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    //add new method findBy(Query Method) CategoryId(Match by categoryId) "id" parameter value
+    //Behind the scenes, Spring will execute a query like this SELECT*FROM product where category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
