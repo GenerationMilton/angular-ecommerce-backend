efd830f3bba5e40343f03c2444c1a604f2591def
milton munoz <livemilton@hotmail.com>
2025-02-26 22:08:09 -0500

    checkout form -save order repository and service 2

--- src/main/java/com/livemilton/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/livemilton/ecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,11 @@ package com.livemilton.ecommerce.service;
 import com.livemilton.ecommerce.dao.CustomerRepository;
 import com.livemilton.ecommerce.dto.Purchase;
 import com.livemilton.ecommerce.dto.PurchaseResponse;
+import com.livemilton.ecommerce.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -15,7 +18,36 @@ public class CheckoutServiceImpl implements CheckoutService{
         this.customerRepository=customerRepository;
     }
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
+
+        //retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        //generate tracking number
+        String orderTrackingNumber= generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        //populate order with orderItems
+
+        //populate order with orderItems
+
+        //populate order with billingAddress
+
+        //populate customer with order
+
+        //save to the database
+
+        //return a response
+
+        return null;
+
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        //generate a random UUID number(UUID version-4)
+        //For details see: https://en.wikipedia.org/wiki/Universally_unique_identifier
         return null;
     }
 
