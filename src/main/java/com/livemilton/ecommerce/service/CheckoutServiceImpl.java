0eaa20e3298b852ec701592aec88d7471af4bd59
milton munoz <livemilton@hotmail.com>
2025-02-26 22:17:59 -0500

    checkout form -save order repository and service 3

--- src/main/java/com/livemilton/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/livemilton/ecommerce/service/CheckoutServiceImpl.java
@@ -4,10 +4,14 @@ import com.livemilton.ecommerce.dao.CustomerRepository;
 import com.livemilton.ecommerce.dto.Purchase;
 import com.livemilton.ecommerce.dto.PurchaseResponse;
 import com.livemilton.ecommerce.entity.Order;
+import com.livemilton.ecommerce.entity.OrderItem;
+import com.livemilton.ecommerce.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -29,18 +33,24 @@ public class CheckoutServiceImpl implements CheckoutService{
         order.setOrderTrackingNumber(orderTrackingNumber);
 
         //populate order with orderItems
+        Set<OrderItem> orderItems= purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
 
-        //populate order with orderItems
-
-        //populate order with billingAddress
+        //populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
 
         //populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
 
         //save to the database
+        customerRepository.save(customer);
 
         //return a response
 
-        return null;
+
+        return new PurchaseResponse(orderTrackingNumber);
 
     }
 
@@ -48,7 +58,7 @@ public class CheckoutServiceImpl implements CheckoutService{
 
         //generate a random UUID number(UUID version-4)
         //For details see: https://en.wikipedia.org/wiki/Universally_unique_identifier
-        return null;
+        return UUID.randomUUID().toString();
     }
 
 
